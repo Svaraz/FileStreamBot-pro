2af64532ae37406c40e6099a8145c2afd8b2729c
Ruban Durai <77948100+RubanDurai@users.noreply.github.com>
2023-10-18 09:38:01 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -16,9 +16,10 @@ class Var(object):
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
-    PORT = int(getenv('PORT', 8080))
+    PORT = int(getenv('PORT', 8080)) 
+    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())  
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes  
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
@@ -37,4 +38,4 @@ class Var(object):
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split()))  
